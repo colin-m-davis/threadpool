ab8b15ba52c3523dd6b1fa723ad850db3626ddd9
colindavis <colinmichaelsdavis@gmail.com>
2023-06-17 11:04:29 -0700

    implement stop_source/stop_tokens

--- src/threadpool.cpp
+++ src/threadpool.cpp
@@ -5,25 +5,30 @@ namespace dreadpools {
 
 ThreadPool::ThreadPool(const std::size_t count_threads) : threads(count_threads) {
     for (auto& t : threads) {
-        t = std::thread(std::thread(ThreadWorker(*this)));
+        t = std::jthread(ThreadWorker(*this));
     }
+}
+
+ThreadPool::~ThreadPool() {
+    stop_source.request_stop();
+    _cv.notify_all();
 }
 
 
 void ThreadWorker::operator()() {
-    std::cout << "operator()\n";
-    // <stop_token>
-    while (true) {
+    while (!stop_token.stop_requested()) {
         std::function<void()> task;
         bool has_task = false;
-        while (!has_task) {
+        while (!has_task && !stop_token.stop_requested()) {
             std::unique_lock lock(pool._cv_m);
             if (pool._tasks.empty()) {
                 pool._cv.wait(lock);
             }
             has_task = (pool._tasks.dequeue(task));
         }
-        task();  // perform task
+        if (!stop_token.stop_requested()) {
+            task();  // perform task
+        }
     }
 }
 
--- src/threadpool.hpp
+++ src/threadpool.hpp
@@ -12,30 +12,36 @@
 #include <stop_token>
 #include "taskqueue.hpp"
 
+
 namespace dreadpools {
 
 class ThreadPool {
 public:
     ThreadPool(const std::size_t count_threads);
     bool is_active{false};
+    ~ThreadPool();
 
 private:
-    std::vector<std::thread> threads;
+    std::vector<std::jthread> threads;
     std::condition_variable _cv{};
     std::mutex _cv_m{};
     TaskQueue<std::function<void()>> _tasks{};
+    std::stop_source stop_source;
     friend class ThreadWorker;
 };
 
 
 class ThreadWorker {
 public:
-    ThreadWorker(ThreadPool& p) : pool(p) {}
+    explicit ThreadWorker(ThreadPool& p) :
+        pool(p),
+        stop_token(p.stop_source.get_token()
+    ) {}
     void operator()();
 
 private:
-    // std::stop_token stop_token;
     ThreadPool& pool;
+    std::stop_token stop_token;
 };
 
 }
