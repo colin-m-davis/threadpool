3931237d89ecf6381dbe08142844b79a30cf333d
colindavis <colinmichaelsdavis@gmail.com>
2023-06-16 23:48:59 -0700

    remove lock typedef (use c++17 type deduction)

--- src/taskqueue.hpp
+++ src/taskqueue.hpp
@@ -9,20 +9,20 @@ namespace dreadpools {
 template <typename T>
 class TaskQueue {
 public:
-    [[nodiscard]] bool empty() const {
-        lock_t lock(_mutex);
+    [[nodiscard]] bool empty() {
+        std::lock_guard lock(_mutex);
         return _queue.empty();
     }
 
     template <typename ... Args>
     void enqueue(Args&& ... args) {
-        lock_t lock(_mutex);
+        std::lock_guard lock(_mutex);
         _queue.emplace(std::forward<T>(args)...);
     }
     
     // caller is responsible for providing out param
     bool dequeue(T& out) {
-        lock_t lock(_mutex);
+        std::lock_guard lock(_mutex);
         if (_queue.empty()) {
             return false;
         }
@@ -34,7 +34,6 @@ public:
 private:
     std::queue<T> _queue{};
     std::mutex _mutex{};
-    typedef std::lock_guard<std::mutex> lock_t;
 };
 
 }
